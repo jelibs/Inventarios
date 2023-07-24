c9c255dedf20a58b9fba35a5fa51f8e9f3ed6621
Jose Bravo <joselbravo1@gmail.com>
2023-07-24 17:49:29 -0600

    Cambio

--- registrar_usuario.php
+++ registrar_usuario.php
@@ -1,19 +1,19 @@
 <?php
 include('conexion.php');
 session_start();
-if (isset($_POST['register'])) {
-    $username = $_POST['username'];
-    $email = $_POST['email'];
+if (isset($_POST['registrar'])) {
+    $nombre = $_POST['nombre'];
+    $correo = $_POST['correo'];
     $password = $_POST['password'];
     $password_hash = password_hash($password, PASSWORD_BCRYPT);
-    $query = $connection->prepare("SELECT * FROM users WHERE EMAIL=:email");
+    $query = $connection->prepare("SELECT * FROM usuarios WHERE correo=:correo");
     $query->bindParam("email", $email, PDO::PARAM_STR);
     $query->execute();
     if ($query->rowCount() > 0) {
-        echo '<p class="error">The email address is already registered!</p>';
+        echo '<p class="error">¡Este correo ya se encuentra registrado!</p>';
     }
     if ($query->rowCount() == 0) {
-        $query = $connection->prepare("INSERT INTO users(USERNAME,PASSWORD,EMAIL) VALUES (:username,:password_hash,:email)");
+        $query = $connection->prepare("INSERT INTO usuarios('0','nombre','correo,'password',now(),1) VALUES (:username,:password_hash,:email)");
         $query->bindParam("username", $username, PDO::PARAM_STR);
         $query->bindParam("password_hash", $password_hash, PDO::PARAM_STR);
         $query->bindParam("email", $email, PDO::PARAM_STR);
